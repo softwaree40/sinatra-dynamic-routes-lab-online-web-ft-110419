7e49c6da1c26caee720c61c1e58e2060644833be
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-01-06 23:40:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -24,7 +24,7 @@ class App < Sinatra::Base
         rent
     end
       get '/say/:word1/:word2/:word3/:word4/:word5' do
-        binding.pry
+        #binding.pry
          params.map{|k,v| v }.join(' ')
       end
       #   get '/:operation/:number1/:number2'
