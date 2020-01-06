563f5b59891dcc9cc47af33a3c42f9f2e8e4441f
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-01-06 23:38:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,11 +25,7 @@ class App < Sinatra::Base
     end
       get '/say/:word1/:word2/:word3/:word4/:word5' do
         binding.pry
-          # @word1 = params[:word1]
-          # @word2 = params[:word2]
-          # @word3 = params[:word3]
-          # @word4 = params[:word4]
-          # @word5 = params[:word5]
+         params.map{|k,v| v }.join(' ')
       end
       #   get '/:operation/:number1/:number2'
       #       @number1 = params[:number1]
