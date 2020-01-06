ec2112e592c6ce741b0bc8f0a70458fed6506a39
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-01-06 23:17:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -22,21 +22,21 @@ class App < Sinatra::Base
         
         
     end
-      get '/say/:word1/:word2/:word3/:word4/:word5' do 
-          @word1 = params[:word1]
-          @word2 = params[:word2]
-          @word3 = params[:word3]
-          @word4 = params[:word4]
-          @word5 = params[:word5]
-      end
-        get '/:operation/:number1/:number2'
-            @number1 = params[:number1]
-            @number2 = params[:number2]
-            # (@number1.to_i + @number2.to_i).to_s
-            # (@number1.to_i - @number2.to_i).to_s
-            # (@number1.to_i * @number2.to_i).to_s
-            # (@number1.to_i / @number2.to_i).to_s
+      # get '/say/:word1/:word2/:word3/:word4/:word5' do 
+      #     @word1 = params[:word1]
+      #     @word2 = params[:word2]
+      #     @word3 = params[:word3]
+      #     @word4 = params[:word4]
+      #     @word5 = params[:word5]
+      # end
+      #   get '/:operation/:number1/:number2'
+      #       @number1 = params[:number1]
+      #       @number2 = params[:number2]
+      #       (@number1.to_i + @number2.to_i).to_s
+      #       (@number1.to_i - @number2.to_i).to_s
+      #       (@number1.to_i * @number2.to_i).to_s
+      #       (@number1.to_i / @number2.to_i).to_s
            
-        end
+      #   end
     
 end
